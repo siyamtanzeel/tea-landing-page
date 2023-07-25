33af4378a8a06049d2619928fb43afe05c8d34c1
siyamtanzeel <sttanzilas@gmail.com>
2023-07-25 18:23:48 +0600

    client section created

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const modeToggle = document.querySelector("#mode-toggle");
 const swap = document.querySelector('.swap');
 const drawerSide = document.querySelector('.drawer-side');
 const myDrawer = document.querySelector('#my-drawer');
+const products = document.querySelectorAll('.card-compact');
+const myModal = document.querySelector('.modal-toggle')
 swap.addEventListener('click',()=>{
     if(modeToggle.checked){
         document.querySelector('html').classList.add('dark');
@@ -10,7 +12,6 @@ swap.addEventListener('click',()=>{
         document.querySelector('html').classList.remove('dark');
     }
 })
-
 // myDrawer.addEventListener('click',()=>{
 //     if(myDrawer.checked){
 //         drawerSide.classList.add('dark:bg-slate-950')
